a270d0fc58aa646372b8dce0055fbe94db30fed8
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-10-11 08:55:31 -0500

    Crea el archivo para parseo del header

---
+++ src/parseHeader.js
@@ -0,0 +1,46 @@
+const toDecimal = (binary) => Number.parseInt(binary, 10);
+
+const getHeader = (binaryString = '') => {
+    const zipStructure = binaryString.slice(0, 8);
+
+    const header = {
+        version: Number.parseInt(zipStructure.slice(0, 3), 10),
+        isZipped: Boolean(zipStructure[3]),
+        fixedZip: Boolean(zipStructure[4]),
+        hasAscii: Boolean(zipStructure[5]),
+        hasAsciiExt: Boolean(zipStructure[6]),
+        hasUnicode: Boolean(zipStructure[7]),
+    };
+
+    const asciiStart = 8;
+    const asciiCount = header.hasAscii
+        ? toDecimal(binaryString.slice(asciiStart, asciiStart + 7))
+        : 0;
+    const asciiExtStart = asciiStart + (header.hasAscii ? 7 : 0);
+    const asciiExtCount = header.hasAsciiExt
+        ? toDecimal(binaryString.slice(asciiExtStart, asciiExtStart + 8))
+        : 0;
+    const unicodeStart = asciiExtStart + (header.hasAsciiExt ? 8 : 0);
+    const unicodeCount = header.hasUnicode
+        ? toDecimal(binaryString.slice(unicodeStart, unicodeStart + 16))
+        : 0;
+
+    const mapStart = unicodeStart + (header.hasUnicode ? 16 : 0);
+
+    return { asciiCount, asciiExtCount, unicodeCount, mapStart };
+};
+//
+// getHeader
+// confirm version
+// load data
+//      compressed
+//      fixed compression
+//      ascii
+//      asciiExt
+//      unicode
+// know length next segment
+// make counts
+//      many ascii
+//      many asciiExt
+//      many unicode
+// know map begining
