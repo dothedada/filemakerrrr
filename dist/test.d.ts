1c045299d3bd1b2f45fd324041a8a09cd99e7480
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-10-22 16:26:42 -0500

    Congigura ts

---
+++ dist/test.d.ts
@@ -0,0 +1,2 @@
+export declare const pato: number[];
+//# sourceMappingURL=test.d.ts.map
---
+++ dist/test.js
@@ -0,0 +1,5 @@
+const suma = (a, b) => {
+    return a + b;
+};
+export const pato = [1, 2, 3, 4];
+console.log(suma(1, 2));
